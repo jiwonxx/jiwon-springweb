22e935f802bb27db4bd1b6ba8c7dc29b0a65bdf6
김지원 <jiwonx_x_@naver.com>
2019-05-31 15:24:58 +0900

    update

--- src/main/java/com/jiwon/article/ArticleController.java
+++ src/main/java/com/jiwon/article/ArticleController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import com.jiwon.book.chap11.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,15 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
-import com.jiwon.book.chap11.Member;
-
 @Controller
 public class ArticleController {
 
 	@Autowired
 	ArticleDao articleDao;
 
-	Logger logger = LogManager.getLogger();
+	static final Logger logger = LogManager.getLogger();
 
 	/**
 	 * 글 목록
@@ -52,13 +51,11 @@ public class ArticleController {
 		model.addAttribute("article", article);
 	}
 
+	/**
+	 * 글 등록 화면
+	 */
 	@GetMapping("/article/addForm")
 	public String articleAddForm(HttpSession session) {
-		Object memberObj = session.getAttribute("MEMBER");
-		if (memberObj == null)
-			// 세션에 MEMBER가 없을 경우 로그인 화면으로
-			return "login/loginForm";
-
 		return "article/addForm";
 	}
 
@@ -67,46 +64,61 @@ public class ArticleController {
 	 */
 	@PostMapping("/article/add")
 	public String articleAdd(Article article,
-			@SessionAttribute("MEMBER") Member member){
+			@SessionAttribute("MEMBER") Member member) {
 		article.setUserId(member.getMemberId());
 		article.setName(member.getName());
 		articleDao.addArticle(article);
 		return "redirect:/app/article/list";
-}
-	
-	
-	@GetMapping("/article/updateform")
-	public String articleUpdateForm(@RequestParam("articleId") String articleId,
+	}
+
+	/**
+	 * 글 수정 화면
+	 */
+	@GetMapping("/article/updateForm")
+	public void updateForm(@RequestParam("articleId") String articleId,
 			@SessionAttribute("MEMBER") Member member, Model model) {
 		Article article = articleDao.getArticle(articleId);
-		if (!article.getUserId().equals(member.getMemberId())) {
-			logger.debug("X", member.getName());
-			return "redirect:/app/article/view?articleId=" + article.getArticleId();
 
-		}
+		// 권한 체크 : 세션의 memberId와 글의 userId를 비교
+		if (!member.getMemberId().equals(article.getUserId()))
+			// 자신의 글이 아니면
+			throw new RuntimeException("No Authority!");
+
 		model.addAttribute("article", article);
-		return "article/updateform";
-}
-	
+	}
+
+	/**
+	 * 글 수정
+	 */
 	@PostMapping("/article/update")
-	public String articleUpdate(Article article) {
-		articleDao.updateArticle(article);
+	public String update(Article article,
+			@SessionAttribute("MEMBER") Member member) {
+		article.setUserId(member.getMemberId());
+		int updatedRows = articleDao.updateArticle(article);
+
+		// 권한 체크 : 글이 수정되었는지 확인
+		if (updatedRows == 0)
+			// 글이 수정되지 않음. 자신이 쓴 글이 아님
+			throw new RuntimeException("No Authority!");
+
 		return "redirect:/app/article/view?articleId=" + article.getArticleId();
 	}
-	
 
-	
+	/**
+	 * 글 삭제
+	 */
 	@GetMapping("/article/delete")
-	public String articleDelete(@RequestParam("articleId") String articleId, 
+	public String delete(@RequestParam("articleId") String articleId,
 			@SessionAttribute("MEMBER") Member member) {
-		Article article = articleDao.getArticle(articleId);
-		if (!article.getUserId().equals(member.getMemberId())) {
-			logger.debug("X", member.getName());
-			return "redirect:/app/article/view?articleId=" + article.getArticleId();
-		}
-		articleDao.deleteArticle(articleId);
-		return "article/delete";
-}
+		int updatedRows = articleDao.deleteArticle(articleId,
+				member.getMemberId());
 
-	
+		// 권한 체크 : 글이 삭제되었는지 확인
+		if (updatedRows == 0)
+			// 글이 삭제되지 않음. 자신이 쓴 글이 아님
+			throw new RuntimeException("No Authority!");
+
+		logger.debug("글을 삭제했습니다. articleId={}", articleId);
+		return "redirect:/app/article/list";
+	}
 }
